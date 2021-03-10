da856aa779d86d13d65c483d96699c08252a4c71
Thomas <toolreaz@outlook.fr>
2021-03-10 09:34:14 +0100

    Update fix-sdhci.sh

--- scripts/fix-sdhci.sh
+++ scripts/fix-sdhci.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "options modprobe sdhci debug_quirks2=0x80000000" > /etc/modprob.d/sdhci-pci.conf
+echo "options modprobe sdhci debug_quirks2=0x80000000" > /etc/modprobe.d/sdhci-pci.conf
